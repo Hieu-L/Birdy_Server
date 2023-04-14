d99a83e531d57b50636399e2cc343702ed42aab6
Hieu LE <118977374+Hieu-L@users.noreply.github.com>
2023-04-14 21:04:29 +0200

    Add files via upload

--- controllers/controller_friendship.js
+++ controllers/controller_friendship.js
@@ -1,5 +1,5 @@
 import Friendship from "../model/Friendship.js";
-
+import User from "../model/User.js";
 
 
 
@@ -11,12 +11,23 @@ export const addFriend = async(req,res,next) =>
     const { friend : friend_username } = req.body;
     const username = req.params.username;
 
+    // test : does username and friend_username exist?
+    let username_ok; let friend_ok;
+
+    try { username_ok = await User.findOne( {username : username} ); }
+    catch(error) { return res.status(500).json({ status : "500", msg : "can't connect to the database" }); }
+
+    if ( !username_ok ) { return res.status(404).json( { status : "404" , msg : "username doesn't exist"} ); } 
+
+    try { friend_ok = await User.findOne( {username : friend_username} ); }
+    catch(error) { return res.status(500).json({ status : "500", msg : "can't connect to the database" }); }
+
+    if ( !friend_ok ) { return res.status(404).json( { status : "404" , msg : "friend username doesn't exist"} ); }     
+
     // test : does friendship exist already?
     let exists;
     try { exists = await Friendship.find( { username: username , friend : friend_username} ); }
     catch(error) { return res.status(500).json({ status : "500", msg : "can't connect to the database" }); }
-
-    console.log(exists)
 
     if (exists !== []) { return res.status(400).json( { status : "400", msg : "you are already friends" } ); }
 
@@ -33,14 +44,91 @@ export const addFriend = async(req,res,next) =>
 
 /* ======== get ALL FRIENDS OF USER in the DATABASE ======== */
 
-export const getAllFriends = async(req,res,next) =>
+export const getListFriends = async(req,res,next) =>
 {
     const username = req.params.username;
     let friendsList;
+
+    // test : does username exist?
+    let username_ok;
+    try { username_ok = await User.findOne( {username : username} ); }
+    catch(error) { return res.status(500).json({ status : "500", msg : "can't connect to the database" }); }
+
+    if ( !username_ok ) { return res.status(404).json( { status : "404" , msg : "username doesn't exist"} ); } 
+
+    // get list of friends
 
     try { friendsList = await Friendship.find( { username: username } ); }
     catch(error) { return res.status(500).json({ status : "500", msg : "can't connect to the database" }); }
 
     return res.status(200).json({ status : "200", friendsList });
 }
+
+
+
+/* ======== check if USERNAME is friends with FRIEND_USERNAME ======== */
+
+export const getFriendRelationship = async(req,res,next) =>
+{
+    const username = req.params.username;
+    const friend_username  = req.params.friend_username;
+
+    // test : does username and friend_username exist?
+
+    let username_ok; let friend_ok;
+
+    try { username_ok = await User.findOne( {username : username} ); }
+    catch(error) { return res.status(500).json({ status : "500", msg : "can't connect to the database" }); }
+
+    if ( !username_ok ) { return res.status(404).json( { status : "404" , msg : "username doesn't exist"} ); } 
+
+    try { friend_ok = await User.findOne( {username : friend_username} ); }
+    catch(error) { return res.status(500).json({ status : "500", msg : "can't connect to the database" }); }
+
+    if ( !friend_ok ) { return res.status(404).json( { status : "404" , msg : "friend username doesn't exist"} ); }     
+
+    // return the friendship status of username and friend_username
+    let isFriends; 
+
+    try { isFriends = await Friendship.findOne( { username: username , friend : friend_username } ); }
+    catch(error) { return res.status(500).json({ status : "500", msg : "can't connect to the database" }); }
+
+    if (isFriends) { return res.status(200).json({ status : "200", msg : "True" }); }
+    else { return res.status(200).json({ status : "200", msg : "False" }); }
+}
+
+export const deleteFriend = async(req,res,next) =>
+{
+    const username = req.params.username;
+    const friend_username  = req.params.friend_username;
+
+    // test : does username and friend_username exist?
+
+    let username_ok; let friend_ok;
+
+    try { username_ok = await User.findOne( {username : username} ); }
+    catch(error) { return res.status(500).json({ status : "500", msg : "can't connect to the database" }); }
+
+    if ( !username_ok ) { return res.status(404).json( { status : "404" , msg : "username doesn't exist"} ); } 
+
+    try { friend_ok = await User.findOne( {username : friend_username} ); }
+    catch(error) { return res.status(500).json({ status : "500", msg : "can't connect to the database" }); }
+
+    if ( !friend_ok ) { return res.status(404).json( { status : "404" , msg : "friend username doesn't exist"} ); } 
+    
+
+    // test : does this friendship between username and friend_username exist?
+    let isFriends; 
+
+    try { isFriends = await Friendship.findOne( { username: username , friend : friend_username } ); }
+    catch(error) { return res.status(500).json({ status : "500", msg : "can't connect to the database" }); }
+
+    if (!isFriends) { return res.status(400).json( { status : "400", msg : "this friendship doesn't exist" } ); }
+
+    try { let ok = Friendship.deleteOne({username : username, friend_username : friend_username}); }
+    catch(error) { return res.status(500).json({ status : "500", msg : "can't connect to the database" }); }
+
+    return res.status(200).json({ status : "200", msg : "Friend removed successfully" });
+}  
+
 
--- routes/friendship_routes.js
+++ routes/friendship_routes.js
@@ -2,7 +2,9 @@ import express from 'express';
 
 import { 
     addFriend , 
-    getAllFriends
+    getListFriends ,
+    getFriendRelationship ,
+    deleteFriend
 } 
 from '../controllers/controller_friendship.js';
 
@@ -11,10 +13,16 @@ from '../controllers/controller_friendship.js';
 const router = express.Router();
 
 
-// [1] POST apifriends/user/{username}/addFriend : create a Friendship
-router.post("/:username/addFriend", addFriend);
+// [1] POST apifriends/user/{username}/friends : create a Friendship
+router.post("/:username/friends", addFriend);
 
-// [2] GET apifriends/user/{username}/all : get all friends of a User
-router.get("/:username/all", getAllFriends);
+// [2] GET apifriends/user/{username}/friends : get all friends of a User
+router.get("/:username/friends", getListFriends);
+
+// [3] GET apifriends/user/{username}/friends/{friendid} : get relationship status of username concerning friend_username
+router.get("/:username/friends/:friend_username",getFriendRelationship);
+
+// [4] DELETE apifriends/user/{username}/friends/{friendid} : remove the friendship of username -> friend_username
+router.delete("/:username/friends/:friend_username",deleteFriend);
 
 export default router;
