1bb387b6764d3ee7da9aaa8bb48b5a37019324fc
Hieu <118977374+Hieu-L@users.noreply.github.com>
2023-05-04 17:35:10 +0200

    Update Logout.js

--- client/src/Authentification/Logout.js
+++ client/src/Authentification/Logout.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
+
+
+axios.defaults.baseURL = 'http://localhost:3001';
 
 function Logout(props) {
+
+  const handleLogout = async(e) => {
+    //e.preventDefault();
+
+    try{ await axios.delete('/api/user/logout').then(res => { console.log(res.data); props.onFormSwitch('login'); }) }
+    catch(e) { console.log(e) }
+}
+
+
+
   return (
     <div>
-        <button className='btn' onClick={() => props.onFormSwitch('login')}>logout</button>
+        <button className='btn' onClick={() => handleLogout()}>logout</button>
     </div>
   )
 }
