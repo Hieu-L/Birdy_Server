2ac35d16ad5acffae8671490dcc1ea004a1bb353
Hieu <118977374+Hieu-L@users.noreply.github.com>
2023-05-04 17:36:33 +0200

    Update PeopleCard.js

--- client/src/PeopleCard.js
+++ client/src/PeopleCard.js
@@ -4,32 +4,34 @@ import { useState } from "react";
 import "./PeopleCard.css"
 import PeopleModal from './PeopleModal';
 import SearchIcon from '@mui/icons-material/Search';
+import axios from 'axios';
+import { useRoutes } from 'react-router-dom';
+
+axios.defaults.baseURL = 'http://localhost:3001';
 
 function People(props) {
 
     const [modalOpened, setModalOpened] = useState(false);
-    
+    const [users, setUsers] = useState([]);
+    const [filter, setFilter] = useState("");
 
-    const [users, setUser] = useState(
-        [
-            {name: "Hugo Victor", pic: "", followStat: true},
-            {name: "Hugo Victooooooor", pic: "", followStat: false},
-            {name: "Zola Émile", pic: "", followStat: true},
-            {name: "IDK YOU", pic:""}
-        ]
-    )
+
+    try { 
+        axios.get('/api/user/all')
+        .then(res => { const listusers = res.data.users; setUsers(listusers) }) 
+    }   catch(e) { console.log("error getting all users") }
+
 
     return (
 
         <div className='people'>
-            <h3>Make new friends !</h3>
+            <h3> All Users </h3>
 
             <div className='searchbar'> 
-                <input type="text" placeholder="Explore"/>
-                <button className='btn'><SearchIcon /></button>
+                <input type="text" placeholder="Explore" onChange={(e) => setFilter(e.target.value)}/>
             </div>
 
-            <div>{ users.map(u =><User homeHandler={(elem) => props.homeHandler(elem)} name={u.name} pic={u.pic} followStat={u.followStat}/>)}</div>
+            <div>{ users.filter(user => user.username.includes(filter)).map(u =><User homeHandler={(elem) => props.homeHandler(elem)} name={u.username} pic={u.pic} followStat={u.followStat}/>)}</div>
             <hr/>
             {props.page !== "modal" ? <span onClick={() => setModalOpened(true)}>Show more</span> : ""}
 
